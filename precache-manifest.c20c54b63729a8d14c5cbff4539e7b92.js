285b9b834bf65ab1721c5d2f4fe774055c310ac0
Vayango <vayango@hotmail.fr>
2020-09-08 21:50:03 +0200

    Updates

---
+++ precache-manifest.c20c54b63729a8d14c5cbff4539e7b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a98ab81818655a1ffe72ef3638d0004",
+    "url": "/netflixCloneApp/index.html"
+  },
+  {
+    "revision": "0109c07f5e6f6a4c9a23",
+    "url": "/netflixCloneApp/static/css/main.6d2e5e23.chunk.css"
+  },
+  {
+    "revision": "2f214a3b7b3b66b62dbe",
+    "url": "/netflixCloneApp/static/js/2.7b10e28f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflixCloneApp/static/js/2.7b10e28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0109c07f5e6f6a4c9a23",
+    "url": "/netflixCloneApp/static/js/main.1aa52b91.chunk.js"
+  },
+  {
+    "revision": "6bcb4c2d75ec26daf892",
+    "url": "/netflixCloneApp/static/js/runtime-main.54351a0e.js"
+  }
+]);
